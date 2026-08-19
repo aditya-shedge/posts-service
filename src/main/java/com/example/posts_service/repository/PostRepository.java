efe17f5e9e77c945155291f7469bd4cb0c582610
Aditya Shedge <aditya.shedge@technogise.com>
2026-08-19 15:40:36 +0530

    feat: add methods to find post by status

--- src/main/java/com/example/posts_service/repository/PostRepository.java
+++ src/main/java/com/example/posts_service/repository/PostRepository.java
@@ -1,13 +1,19 @@
 package com.example.posts_service.repository;
 
 import com.example.posts_service.model.Post;
+import com.example.posts_service.model.PostStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByOrderByCreatedAtDesc();
+
+    List<Post> findAllByStatusOrderByCreatedAtDesc(PostStatus status);
+
+    Optional<Post> findByIdAndStatus(UUID id, PostStatus status);
 }
