29858d24e095c7591e5a8bb29a30344be211692e
Aditya Shedge <aditya.shedge@technogise.com>
2026-08-20 10:23:02 +0530

    feat: add swagger open api config

---
+++ src/main/java/com/example/posts_service/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.example.posts_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Posts Service API")
+                        .description("REST API for managing school announcements and posts")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Posts Service Team")
+                                .email("support@school.edu")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
